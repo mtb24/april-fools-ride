13e0f6446751139dcc5cdfd8ba4770496480126a
mtb24 <kendowney.com@gmail.com>
2018-04-12 11:19:52 -0700

    comment out FB login for now

--- src/nav.js
+++ src/nav.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './nav.css';
-import {facebookLogin} from './social-login';
+//import {facebookLogin} from './social-login';
 
 
 class Nav extends Component {
     render() {
         return (
             <div className="nav-list">
-                <button id="fblogin" onClick={facebookLogin}>Login with FB</button>
+                {/* <button id="fblogin" onClick={facebookLogin}>Login with FB</button> */}
             </div>
         );
     }
